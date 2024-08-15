4edf97f4eaa9978fa1bf0686bf7363cc676ca9ec
ManasaReddy3347 <manasareddy200322@gmail.com>
2024-08-15 10:54:42 +0530

    Update unique_elements.py

--- unique_elements.py
+++ unique_elements.py
@@ -1,11 +1,11 @@
-arr = [1, 2, 3, 2, 1, 4, 5, 4]
-seen = set()
-unique_elements = []
-
-for element in arr:
-    if element not in seen:
-        unique_elements.append(element)
-        seen.add(element)
-
-for element in unique_elements:
-    print(element)
+n=int(input())
+arr=list(map(int,input().split()))
+l=[]
+for i in range(n):
+    c=0
+    for j in range(n):
+        if arr[i]==arr[j]:
+            c+=1
+    if c==1:
+        l.append(arr[i])
+print(" ".join(map(str,l)))
